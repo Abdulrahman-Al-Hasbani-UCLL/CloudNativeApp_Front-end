58f230ad3ea8e82e9898820bfa7c6a85ca7c1ee6
Abdulrahman-Al-Hasbani-UCLL <abdulrahman.alhasbani@student.ucll.be>
2025-05-20 20:20:08 +0200

    Login and logout work

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Sidebar = ({ data }) => {
     const [isMobileNavHidden, setIsMobileNavHidden] = useState(true);
+    const [loggedinUser, setLoggedInUser] = useState(false);
+    const router = useRouter();
+    useEffect( () => {
+        const loginToken = localStorage.getItem("forumUserToken");
+        if (loginToken) {
+            setLoggedInUser(true);
+        } else {
+            setLoggedInUser(false);
+        }
+    }, [])
 
     const sidebarHandler = () => {
         setIsMobileNavHidden(!isMobileNavHidden);
@@ -59,26 +70,57 @@ const Sidebar = ({ data }) => {
                         cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-6 py-1
                         hover:text-blue-700 focus:text-blue-700 focus:outline-none flex items-center"
                     >
-                        <Link href={data?.id ? `/logout` : `/login`}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="icon icon-tabler icon-tabler-users"
-                                width={20}
-                                height={20}
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                fill="none"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
+                        {loggedinUser ? (
+                            <button
+                                onClick={() => {
+                                    setLoggedInUser(false);
+                                    localStorage.removeItem("forumUserToken");
+                                    alert("Logout sucessfull");
+                                    router.push('/login');
+                                }}
+                                className="flex items-center"
                             >
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <circle cx={9} cy={7} r={4} />
-                                <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                                <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                                <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
-                            </svg>
-                        </Link>
+                                {/* Logout icon */}
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="icon icon-tabler icon-tabler-logout"
+                                    width={20}
+                                    height={20}
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                    <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                                    <path d="M9 12h12l-3 -3" />
+                                    <path d="M18 15l3 -3" />
+                                </svg>
+                            </button>
+                        ) : (
+                            <Link href="/login" className="flex items-center">
+                                {/* Login/Register icon */}
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="icon icon-tabler icon-tabler-login"
+                                    width={20}
+                                    height={20}
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                >
+                                    <path stroke="none" d="M0 0h24v24H0z" />
+                                    <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                                    <path d="M9 12h12l-3 -3" />
+                                    <path d="M18 15l3 -3" />
+                                </svg>
+                            </Link>
+                        )}
                     </li>
                 </ul>
             </div>
@@ -122,14 +164,27 @@ const Sidebar = ({ data }) => {
                             Home
                         </Link>
                     </li>
+                    
                     <li className="
                         cursor-pointer text-gray-600 text-sm tracking-normal mt-2 py-2
                         hover:text-blue-700 focus:text-blue-700 focus:outline-none flex items-center"
                     >
-                        {data?.id ? (
-                            <Link href="/logout" className="ml-2">Logout</Link>
+                        {loggedinUser ? (
+                            <button
+                                onClick={() => {
+                                    setLoggedInUser(false);
+                                    localStorage.removeItem("forumUserToken")
+                                    alert("Logout sucessfull");
+                                    router.push('/login');
+                                }}
+                                className="flex items-center"
+                            >
+                                <span className="ml-2">Logout</span>
+                            </button>
                         ) : (
-                            <Link href="/login" className="ml-2 leading-relaxed">Login/Register</Link>
+                            <Link href="/login" className="flex items-center">
+                                <span className="ml-2">Login/Register</span>
+                            </Link>
                         )}
                     </li>
                 </ul>
