853b2b143e3b688884f15ff28096fd100afaf0b0
Abdulrahman-Al-Hasbani-UCLL <abdulrahman.alhasbani@student.ucll.be>
2025-05-24 22:41:31 +0200

    Fixed dynamic routes to prevent error

--- pages/thread/index.js
+++ pages/thread/index.js
@@ -1,14 +1,13 @@
-import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import ThreadComp from '@/components/threads/threadComp';
 
 const Thread = () => {
     const router = useRouter();
+    const { id } = router.query;
 
-    useEffect(() => {
-        router.push('/');
-    }, [router]);
+    if (!id) return <div>Loading...</div>;
 
-    return null;
+    return <ThreadComp id={id} />;
 };
 
 export default Thread;
